0c2b1fb6e8a19212177bbeb4f3bf126df497d2d1
taishi.kitamura <taisikitamra@gmail.com>
2025-03-28 11:35:49 +0900

    add vercel.json for change vercel deploy root src/app

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,11 @@ const nextConfig: NextConfig = {
         hostname: "images.microcms-assets.io"
       }
     ]
-  }
+  },
+  //  // "@ts-expect-error
+  //  experimental: {
+  //   appDir: true, // `src/app` を有効にする
+  // },
 };
 
 export default nextConfig;
