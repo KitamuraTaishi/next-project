940f06270189f8ec3f9af38128372e18003f5901
taishi.kitamura <taisikitamra@gmail.com>
2025-03-28 15:45:21 +0900

    add Suspendse for client component

--- src/components/SearchField/index.tsx
+++ src/components/SearchField/index.tsx
@@ -2,34 +2,49 @@
 import Image from "next/image";
 import styles from "./index.module.css"
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from 'react';
 
 
-export default function SearchField() {
-    const router = useRouter()
-    const searchParams = useSearchParams()
-
-    const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const q = e.currentTarget.elements.namedItem("q")
-        if (q instanceof HTMLInputElement) {
-            const params = new URLSearchParams()
-            params.set("q", q.value.trim())
-            console.log(params.toString())
-            router.push(`/news/search?${params.toString()}`)
-        }
-    }
+function SearchFieldComponent() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+  
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const q = e.currentTarget.elements.namedItem('q');
+      if (q instanceof HTMLInputElement) {
+        const params = new URLSearchParams();
+        params.set('q', q.value.trim());
+        router.push(`/news/search?${params.toString()}`);
+      }
+    };
+  
     return (
-        <form className={styles.from} onSubmit={handlerSubmit}>
-            <label className={styles.search}>
-                <Image src="/search.svg" alt="" width={16} height={16} loading="eager"/>
-                <input 
-                    type="text"
-                    name="q"
-                    defaultValue={searchParams.get("q") ?? undefined}
-                    placeholder="キーワードを入力"
-                    className={styles.searchInput}
-                />
-            </label>
-        </form>
-    )
-}
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label className={styles.search}>
+          <Image
+            src="/search.svg"
+            alt="検索"
+            width={16}
+            height={16}
+            loading="eager"
+          />
+          <input
+            type="text"
+            name="q"
+            defaultValue={searchParams.get('q') ?? undefined}
+            placeholder="キーワードを入力"
+            className={styles.searchInput}
+          />
+        </label>
+      </form>
+    );
+  }
+  
+  export default function SearchField() {
+    return (
+      <Suspense fallback={<p>Loading...</p>}>
+        <SearchFieldComponent />
+      </Suspense>
+    );
+  }
